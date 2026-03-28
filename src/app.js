99c2f88ffd8097179dc7ce59afff0bdb951df35a
md . ahsan khan <sk6582757@gmail.com>
2026-03-29 04:13:11 +0530

    changed origing to localhost

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors({
-    origin:"https://moodifymusics.netlify.app",
+    origin:"http://localhost:5173",
     credentials:true,
     methods:["POST","PATCH","DELETE","GET"]
 }))
