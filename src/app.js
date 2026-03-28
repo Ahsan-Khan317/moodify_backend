2b47bfa4930d4776867fb0c9735876e4c8be0ac8
md . ahsan khan <sk6582757@gmail.com>
2026-03-29 04:24:27 +0530

    added live origin to cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://moodifymusics.netlify.app",
     credentials:true,
     methods:["POST","PATCH","DELETE","GET"]
 }))
