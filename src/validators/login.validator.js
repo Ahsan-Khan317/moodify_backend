c187e420f2383cb2823705627d5dd3e47e0192f8
md . ahsan khan <sk6582757@gmail.com>
2026-03-31 23:58:53 +0530

    corrected typo in the msg content

--- src/validators/login.validator.js
+++ src/validators/login.validator.js
@@ -4,7 +4,7 @@ const login_validator = [
  
     body("email").trim().notEmpty().withMessage("email is required for login").bail().isEmail().withMessage("Invalid Email"),
 
-    body("password").trim().notEmpty().withMessage("password required for login").bail().isLength({min:5,max:12}).withMessage("password should e between 6 to 12 character")
+    body("password").trim().notEmpty().withMessage("password required for login").bail().isLength({min:5,max:12}).withMessage("password should be between 6 to 12 character")
 
 
 
